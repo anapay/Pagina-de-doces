013dcc91855042aebf0637c3d2ee663e1857fce1
anapay <anapay42@gmail.com>
2020-06-18 19:00:06 -0300

    Sequelize

---
+++ arquivo.sql
@@ -0,0 +1,30 @@
+
+CREATE TABLE produtos(
+    codigo INT,
+    nome VARCHAR(255),
+    valor Float
+);
+
+INSERT INTO produtos(codigo, nome, valor) VALUES(
+    1234,
+    "brigadeiro",
+    8.90
+);
+
+INSERT INTO produtos(codigo, nome, valor) VALUES(
+    1235,
+    "beijinho",
+    8.90
+);
+
+INSERT INTO produtos(codigo, nome, valor) VALUES(
+    1235,
+    "bemcasado",
+    8.90
+);
+
+INSERT INTO produtos(codigo, nome, valor) VALUES(
+    1235,
+    "strawberry",
+    8.90
+);
