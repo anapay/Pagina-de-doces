0fb22f34360f785d655b2434c0de53677a2a6657
anapay <anapay42@gmail.com>
2020-07-02 19:03:23 -0300

    Deletando arquivos

--- index.js
+++ index.js
@@ -13,15 +13,7 @@ app.set("view engine", "handlebars")
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-// rotas
-app.get("/", function (req, res) {
-    Cliente.findAll({raw: true}).then(function(clientes){
-        // console.log(clientes)
-        res.render("home", {clientes: clientes})
-
-    })
- 
-})
+//rotas
 app.get("/cadastro", function (req, res) {
     res.render("formulario")
 })
@@ -40,6 +32,31 @@ app.post("/Received", function (req, res) {
     })
 
 })
+// rotas
+app.get("/", function (req, res) {
+    Cliente.findAll({ raw: true }).then(function (clientes) {
+        // console.log(clientes)
+        res.render("home", { clientes: clientes })
+
+    })
+
+})
+app.get("/clientes", function (req, res) {
+    Cliente.findAll({ raw: true }).then(function (clientes) {
+        Cliente.findAll({ order: [["id", "DESC"]] })
+
+        res.render("lista", { clientes: clientes })
+    })
+
+})
+app.get("/deletar/:id", function(req, res){
+    Cliente.destroy({where: {"id": req.params.id}}).then(function(){
+        res.send("Deletado com sucesso!")
+    }).catch(function(erro){
+        res.send("Cliente não existe")
+    })
+})
+
 
 
 app.listen(8888, function () {
